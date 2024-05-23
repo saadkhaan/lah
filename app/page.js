589b1bcf8cf7b6a2f58b7624f7cf3155a3efba53
Saad Khaan <saad@pixelsbit.com>
2024-05-23 13:35:49 +0400

    styles moved to global css

--- app/page.js
+++ app/page.js
@@ -4,12 +4,8 @@ export default function Home() {
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center justify-center p-24">
-				<h1 className="font-black text-6xl font-heading dark:text-slate-500">
-					Laith Abdul Hadi
-				</h1>
-				<p className="text-gray-500 tracking-wide dark:text-slate-400">
-					website coming soon
-				</p>
+				<h1 className="">Laith Abdul Hadi</h1>
+				<p className="">website coming soon</p>
 			</main>
 		</>
 	);
