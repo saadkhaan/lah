f5d299f52abd29eaca836475a25f8f0fd7fd5f16
Saad Khaan <saad@pixelsbit.com>
2024-05-28 08:48:55 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,39 +1,15 @@
 import React from "react";
-import coverImage from "../public/cover.jpg";
+// import coverImage from "../public/cover.jpg";
 import HeroCarousel from "@/components/HeroCarousel";
 
-// const pageData = async () => {
-// 	const version = process.env.VERSION;
-// 	const token = process.env.TOKEN;
-// 	const url = `https://api.storyblok.com/v2/cdn/stories/home-page?version=${version}&token=${token}&cv=1716869744`;
-// 	let req = await fetch(url, { cache: "no-store" });
-
-// 	const storyData = await req.json();
-// 	const { hero_section } = storyData.story.content;
-
-// 	return {
-// 		hero_section: hero_section[0],
-// 	};
-// };
-
-export default async function Home() {
+export default function Home() {
 	return (
 		<>
 			<main className="min-h-screen">
 				<section className="hero h-screen">
-					{/* <h1>Story: {hero_section.headline}</h1> */}
 					<HeroCarousel />
 				</section>
 			</main>
 		</>
 	);
-}
-
-export async function fetchData() {
-	let sbParams = { version: "draft" };
-
-	const storyblokApi = getStoryblokApi();
-	return storyblokApi.get(`cdn/stories/home`, sbParams, {
-		cache: "no-store",
-	});
 }
