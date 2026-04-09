a9610d1861a5132e63c54faee45326398146926d
Saad Khaan <khaansaad@outlook.com>
2026-04-09 20:13:01 +0400

    dark mode colors fixed

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -4,31 +4,31 @@ import { useTheme } from "next-themes";
 import { FiMoon, FiSun } from "react-icons/fi";
 
 const ThemeSwitch = () => {
-	const [mounted, setMounted] = useState(false);
-	const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-	// useEffect only runs on the client, so now we can safely show the UI
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-	if (!mounted) {
-		return null;
-	}
+  if (!mounted) {
+    return null;
+  }
 
-	return (
-		<button
-			className="focus:outline-none"
-			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-			aria-label="Change color mode"
-		>
-			{theme === "dark" ? (
-				<FiSun size={24} className="sm:text-slate-950 dark:text-slate-800" />
-			) : (
-				<FiMoon size={24} className="sm:text-slate-950 dark:text-slate-800" />
-			)}
-		</button>
-	);
+  return (
+    <button
+      className="focus:outline-none"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      aria-label="Change color mode"
+    >
+      {theme === "dark" ? (
+        <FiSun size={24} className="sm:text-slate-950 dark:text-slate-400" />
+      ) : (
+        <FiMoon size={24} className="sm:text-slate-950 dark:text-slate-400" />
+      )}
+    </button>
+  );
 };
 
 export default ThemeSwitch;
