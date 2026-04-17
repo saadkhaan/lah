9d9b840a32f2f4f05b2101e1c3f3970ab2346337
Saad Khaan <khaansaad@outlook.com>
2026-04-17 20:00:19 +0400

    Add files via upload

---
+++ app/projects/jeddah-home-v2/page.js
@@ -0,0 +1,213 @@
+import Footer from "@/components/Footer";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaChevronRight } from "react-icons/fa6";
+import { FaChevronLeft } from "react-icons/fa6";
+import { FaChevronDown } from "react-icons/fa6";
+
+export default function page() {
+  return (
+    <>
+      <div className="px-4 project-top">
+        <div className="relative h-52 md:h-[90svh] hidden sm:block">
+          <div className="relative z-10 h-full flex items-center justify-center">
+            <h1 className="bg-slate-50/50 w-full text-center py-4">
+              Jeddah Home, Saudi Arabia
+            </h1>
+            <div class="arrow bounce">
+              <a
+                href="#project-details"
+                className="bg-slate-50/60 hover:bg-slate-50 transition-all duration-300"
+              >
+                <FaChevronDown />
+              </a>
+            </div>
+          </div>
+          <Image
+            src="/projects/JeddahHomeSaudiArabia-v2/MAIN-ENTRANCE-LOBBY-V1.png"
+            fill
+            alt=""
+            className="object-cover"
+          />
+        </div>
+        <div className="container" id="project-details">
+          <div className="grid md:grid-cols-2 mt-8 gap-8">
+            <h1 className="sm:hidden">Jeddah Home, Saudi Arabia</h1>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MAIN-LOBBY-V2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MANCAVE-V2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MANCAVE-V4.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/BAR-STAIRCASE-V1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/BAR-STAIRCASE-V2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/DINING-AREA-V1-OPT-1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/DINING-AREA-V2-OPT1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/FAMILY-SITTING-V1-OPT2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/FAMILY-SITTING-V2-OPT2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/FORMAL-LOUNGE-V1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/FORMAL-LOUNGE-V2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/FORMAL-LOUNGE-V3.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MASTERSUIT-HALL-V1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MASTERSUIT-V1.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MASTERSUIT-V2.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+            <div className="project-image-container">
+              <Image
+                src="/projects/JeddahHomeSaudiArabia-v2/MASTERSUIT-V3.png"
+                width={800}
+                height={0}
+                alt=""
+                className="mx-auto"
+              />
+            </div>
+          </div>
+
+          <div className="bottom-navigation flex justify-between">
+            <div className="prev-project my-20">
+              <h3 className="md:text-3xl text-slate-400 hover:text-slate-950 font-bold">
+                <Link
+                  href="../projects/elizabeth-street"
+                  className="inline-flex items-center group transition-all transform mr-2"
+                >
+                  <FaChevronLeft className="mr-2 group-hover:-translate-x-2 text-xl transition" />
+                  Previous project: Elizabeth Street, London{" "}
+                </Link>
+              </h3>
+            </div>
+            <div className="next-project my-20">
+              <h3 className="md:text-3xl text-slate-400 hover:text-slate-950 font-bold">
+                <Link
+                  href="../projects/monte-carlo"
+                  className="inline-flex items-center group transition-all transform mr-2"
+                >
+                  Next project: Monte Carlo, Monaco{" "}
+                  <FaChevronRight className="ml-2 group-hover:translate-x-2 text-xl transition" />
+                </Link>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
