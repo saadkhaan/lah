e12d6a46fc1e1a3b27e52cfd34e992d2f397e9ce
Saad Khaan <khaansaad@outlook.com>
2026-04-22 21:07:14 +0400

    Add files via upload

--- app/bio/page.js
+++ app/bio/page.js
@@ -27,6 +27,8 @@ export default function page() {
             sourced pieces come together to create interiors that feel collected
             rather than staged. Rejecting trends in favor of timelessness, Laith
             designs spaces that are both sophisticated and effortlessly livable.
+          </p>
+          <p className="text-gray-900 text-lg mt-4">
             Laith’s creative foundation began in Boston, where he studied film
             at Emerson College, an experience that shaped his understanding of
             composition, lighting, and storytelling. Early in his career, he
@@ -34,12 +36,14 @@ export default function page() {
             immersive environments that felt cinematic and transformative. These
             experiences played a pivotal role in developing his signature
             aesthetic, where spaces are composed with depth, atmosphere, and a
-            sense of narrative. Today, Laith approaches each project as a
-            personal story, working closely with clients to create interiors
-            that reflect their lifestyle and identity. His designs balance
-            structure and spontaneity, blending classic influences with
-            contemporary living to achieve spaces that are warm, elegant, and
-            enduring.
+            sense of narrative.
+          </p>
+          <p className="text-gray-900 text-lg mt-4">
+            Today, Laith approaches each project as a personal story, working
+            closely with clients to create interiors that reflect their
+            lifestyle and identity. His designs balance structure and
+            spontaneity, blending classic influences with contemporary living to
+            achieve spaces that are warm, elegant, and enduring.
           </p>
         </div>
       </div>
