f7a0a8ee1d6b1e6751c912ccaf010985dc249173
Saad Khaan <khaansaad@outlook.com>
2026-05-14 10:02:09 +0400

    Added padding in x axis

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   const currentPath = usePathname();
   return (
     <>
-      <nav className="w-full bg-white top-0 z-20 fixed ">
+      <nav className="w-full bg-white top-0 z-20 fixed px-4">
         <div className="container mx-auto justify-between px-4 sm:px-0 md:flex md:items-center">
           <div className="flex items-center justify-between py-3 md:block md:py-5">
             <div className="w-32 h-auto md:w-40 font-black md:text-slate-950 text-2xl">
