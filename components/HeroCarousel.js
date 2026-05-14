d454d29050bb8e20a43fa7befeedb645fd9a60ea
Saad Khaan <khaansaad@outlook.com>
2026-05-14 10:02:27 +0400

    Belgravia Mews image updated in the home page slider

--- components/HeroCarousel.js
+++ components/HeroCarousel.js
@@ -50,7 +50,7 @@ export default function HeroCarousel() {
       <swiper-slide>
         <div className="relative block w-screen h-screen h-screen-mobile">
           <Image
-            src="/projects/BelgraviaMewsLondon/Mews2.jpg"
+            src="/projects/BelgraviaMewsLondon/Mews9.jpg"
             alt="a room"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 20vw, 100vw"
             fill
